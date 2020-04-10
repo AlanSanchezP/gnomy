e889631ce37f083a591393c862ec275746765343
Alan Sanchez <sanchezpineda03@gmail.com>
2020-04-10 17:53:35 -0500

    New Account activity: Basic XML

---
+++ app/src/main/java/io/github/alansanchezp/gnomy/ui/account/NewAccountActivity.java
@@ -0,0 +1,15 @@
+package io.github.alansanchezp.gnomy.ui.account;
+
+import androidx.appcompat.app.AppCompatActivity;
+import io.github.alansanchezp.gnomy.R;
+
+import android.os.Bundle;
+
+public class NewAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_account);
+    }
+}
