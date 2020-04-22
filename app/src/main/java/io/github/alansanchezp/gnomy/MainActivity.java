982b89354b53977f53b5c2b758ed0d71e54f08cc
Alan Sanchez <sanchezpineda03@gmail.com>
2020-04-22 18:02:58 -0500

    Main navigation changes toolbar color and icons

--- app/src/main/java/io/github/alansanchezp/gnomy/MainActivity.java
+++ app/src/main/java/io/github/alansanchezp/gnomy/MainActivity.java
@@ -1,6 +1,7 @@
 package io.github.alansanchezp.gnomy;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
 
@@ -8,9 +9,11 @@ import com.jakewharton.threetenabp.AndroidThreeTen;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.graphics.drawable.DrawableCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
+import android.view.Menu;
 import android.view.View;
 import android.view.MenuItem;
 
@@ -22,32 +25,33 @@ import io.github.alansanchezp.gnomy.ui.account.NewAccountActivity;
 
 public class MainActivity extends AppCompatActivity implements AccountsFragment.OnListFragmentInteractionListener {
     private static final String FRAGMENT_TAG = "GNOMY_MAIN_FRAGMENT";
+    private Menu menu;
+    private String currentTitle;
 
     private final BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            Fragment fragment;
-            String title;
+            Fragment fragment = null;
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    title = getString(R.string.title_home);
-                    return true;
+                    currentTitle = getString(R.string.title_home);
+                    break;
                 case R.id.navigation_transactions:
-                    title = getString(R.string.title_transactions);
-                    return true;
+                    currentTitle = getString(R.string.title_transactions);
+                    break;
                 case R.id.navigation_accounts:
-                    title = getString(R.string.title_accounts);
-                    fragment = AccountsFragment.newInstance(1);
+                    currentTitle = getString(R.string.title_accounts);
                     break;
                 case R.id.navigation_notifications:
-                    title = getString(R.string.title_notifications);
-                    return true;
+                    currentTitle = getString(R.string.title_notifications);
+                    break;
                 default:
+                    currentTitle = "";
                     return false;
             }
-            return switchToFragment(fragment, title);
+            return switchFragment();
         }
     };
 
@@ -60,28 +64,69 @@ public class MainActivity extends AppCompatActivity implements AccountsFragment.
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        // Remove default title text
-        setTitle(getString(R.string.title_home));
+
+        currentTitle = getResources().getString(R.string.title_home);
+        setTitle();
 
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
-    public boolean switchToFragment(Fragment fragment, String title) {
-        if (fragment == null) return false;
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        this.menu = menu;
+        getMenuInflater().inflate(R.menu.main_activity_toolbar, menu);
+        tintMenuItems();
+        toggleTransactionActions();
+        return true;
+    }
 
+    private void toggleTransactionActions() {
+        if (currentTitle.equals(getString(R.string.title_transactions))) {
+            menu.findItem(R.id.action_search).setVisible(true);
+            menu.findItem(R.id.action_filter).setVisible(true);
+        } else {
+            menu.findItem(R.id.action_search).setVisible(false);
+            menu.findItem(R.id.action_filter).setVisible(false);
+        }
+    }
+
+    private void tintMenuItems() {
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        MenuItem filterItem = menu.findItem(R.id.action_filter);
+
+        Drawable normalDrawable = searchItem.getIcon();
+        Drawable wrapDrawable = DrawableCompat.wrap(normalDrawable);
+        DrawableCompat.setTint(wrapDrawable, this.getResources().getColor(R.color.colorTextInverse));
+
+        normalDrawable = filterItem.getIcon();
+        wrapDrawable = DrawableCompat.wrap(normalDrawable);
+        DrawableCompat.setTint(wrapDrawable, this.getResources().getColor(R.color.colorTextInverse));
+    }
+
+    public boolean switchFragment() {
         FragmentManager manager = getSupportFragmentManager();
+        Fragment fragment;
+
+        // TODO: Move this two lines right before return statement when all fragments are handled
+        setTitle();
+        toggleTransactionActions();
+
+        if (currentTitle.equals(getResources().getString(R.string.title_accounts))) {
+            fragment = AccountsFragment.newInstance(1);
+        } else {
+            return true;
+        }
 
         manager.beginTransaction()
                 .replace(R.id.main_container, fragment, FRAGMENT_TAG)
                 .commit();
-        setTitle(title);
 
         return true;
     }
 
-    public void setTitle(String title) {
-        getSupportActionBar().setTitle(title);
+    private void setTitle() {
+        getSupportActionBar().setTitle(currentTitle);
     }
 
     public void onFABClick(View v) {
