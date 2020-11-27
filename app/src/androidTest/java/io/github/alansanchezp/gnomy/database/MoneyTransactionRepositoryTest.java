31c29bcc64b2bf45585dedd5879945fa19ea860a
Alan Sanchez <sanchezpineda03@gmail.com>
2020-11-27 17:45:19 -0600

    Code style refactor on MoneyTransactionRepositoryTest

--- app/src/androidTest/java/io/github/alansanchezp/gnomy/database/MoneyTransactionRepositoryTest.java
+++ app/src/androidTest/java/io/github/alansanchezp/gnomy/database/MoneyTransactionRepositoryTest.java
@@ -11,7 +11,6 @@ import org.junit.runner.RunWith;
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.platform.app.InstrumentationRegistry;
-import io.github.alansanchezp.gnomy.LiveDataTestUtil;
 import io.github.alansanchezp.gnomy.database.account.Account;
 import io.github.alansanchezp.gnomy.database.account.AccountRepository;
 import io.github.alansanchezp.gnomy.database.account.MonthlyBalance;
@@ -20,6 +19,7 @@ import io.github.alansanchezp.gnomy.database.transaction.MoneyTransactionReposit
 import io.github.alansanchezp.gnomy.util.BigDecimalUtil;
 import io.github.alansanchezp.gnomy.util.DateUtil;
 
+import static io.github.alansanchezp.gnomy.LiveDataTestUtil.getOrAwaitValue;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
@@ -38,11 +38,13 @@ public class MoneyTransactionRepositoryTest {
                 InstrumentationRegistry.getInstrumentation().getContext());
         AccountRepository accountRepository = new AccountRepository(
                 InstrumentationRegistry.getInstrumentation().getContext());
-        // Create a monthly balance row with predefined values.
+        // Create an empty account
         Account account = new Account();
         account.setDefaultCurrency("USD");
         account.setCreatedAt(DateUtil.OffsetDateTimeNow().minusMonths(2));
         accountRepository.insert(account).blockingGet();
+
+        // Update initial balance with predefined values
         MonthlyBalance firstBalance = new MonthlyBalance();
         firstBalance.setAccountId(1);
         firstBalance.setDate(DateUtil.now().minusMonths(2));
@@ -51,6 +53,7 @@ public class MoneyTransactionRepositoryTest {
         firstBalance.setProjectedIncomes(BigDecimalUtil.fromString("425"));
         firstBalance.setProjectedExpenses(BigDecimalUtil.fromString("100"));
         accountRepository.update(firstBalance).blockingGet();
+
         // Insert second account (for update testing)
         accountRepository.insert(account).blockingGet();
     }
@@ -65,72 +68,53 @@ public class MoneyTransactionRepositoryTest {
     public void custom_insert_method_works() throws InterruptedException {
         MonthlyBalance resultBalance;
         // New transaction at existing monthly balance date
-        MoneyTransaction testTransaction = new MoneyTransaction();
-        testTransaction.setAccount(1);
-        testTransaction.setCategory(1);
-        testTransaction.setDate(DateUtil.OffsetDateTimeNow().minusMonths(2));
-        testTransaction.setCurrency("USD");
-        testTransaction.setOriginalValue("567");
+        MoneyTransaction testTransaction = getDefaultTestTransaction();
 
         // Confirmed income
-        testTransaction.setType(MoneyTransaction.INCOME);
-        testTransaction.setConfirmed(true);
         repository.insert(testTransaction).blockingGet();
-        resultBalance = LiveDataTestUtil.getOrAwaitValue(
+        resultBalance = getOrAwaitValue(
                 repository.getBalanceFromMonth(1, DateUtil.now().minusMonths(2)));
-        // 30 + 567 = 597
-        assertEquals(BigDecimalUtil.fromString("597"), resultBalance.getTotalIncomes());
-        // 172 (stays the same)
-        assertEquals(BigDecimalUtil.fromString("172"), resultBalance.getTotalExpenses());
-        // 425 (stays the same)
-        assertEquals(BigDecimalUtil.fromString("425"), resultBalance.getProjectedIncomes());
-        // 100 (stays the same)
-        assertEquals(BigDecimalUtil.fromString("100"), resultBalance.getProjectedExpenses());
+        testResultBalance(resultBalance,
+                "597", // 30 + 567 = 597
+                "172", // (stays the same)
+                "425", // (stays the same)
+                "100"); // (stays the same)
 
         // Confirmed expense
         testTransaction.setType(MoneyTransaction.EXPENSE);
         testTransaction.setConfirmed(true);
         repository.insert(testTransaction).blockingGet();
-        resultBalance = LiveDataTestUtil.getOrAwaitValue(
+        resultBalance = getOrAwaitValue(
                 repository.getBalanceFromMonth(1, DateUtil.now().minusMonths(2)));
-        // 597 (stays the same)
-        assertEquals(BigDecimalUtil.fromString("597"), resultBalance.getTotalIncomes());
-        // 172 + 567 = 769
-        assertEquals(BigDecimalUtil.fromString("739"), resultBalance.getTotalExpenses());
-        // 425 (stays the same)
-        assertEquals(BigDecimalUtil.fromString("425"), resultBalance.getProjectedIncomes());
-        // 100 (stays the same)
-        assertEquals(BigDecimalUtil.fromString("100"), resultBalance.getProjectedExpenses());
+        testResultBalance(resultBalance,
+                "597", // (stays the same)
+                "739", // 172 + 567 = 769
+                "425", // (stays the same)
+                "100"); // (stays the same)
 
         // Pending income
         testTransaction.setType(MoneyTransaction.INCOME);
         testTransaction.setConfirmed(false);
         repository.insert(testTransaction).blockingGet();
-        resultBalance = LiveDataTestUtil.getOrAwaitValue(
+        resultBalance = getOrAwaitValue(
                 repository.getBalanceFromMonth(1, DateUtil.now().minusMonths(2)));
-        // 597 (stays the same)
-        assertEquals(BigDecimalUtil.fromString("597"), resultBalance.getTotalIncomes());
-        // 739 (stays the same)
-        assertEquals(BigDecimalUtil.fromString("739"), resultBalance.getTotalExpenses());
-        // 425 + 567 = 992
-        assertEquals(BigDecimalUtil.fromString("992"), resultBalance.getProjectedIncomes());
-        // 100 (stays the same)
-        assertEquals(BigDecimalUtil.fromString("100"), resultBalance.getProjectedExpenses());
+        testResultBalance(resultBalance,
+                "597", // (stays the same)
+                "739", // (stays the same)
+                "992", // 425 + 567 = 992
+                "100"); // (stays the same)
 
         // Pending expense
         testTransaction.setType(MoneyTransaction.EXPENSE);
         testTransaction.setConfirmed(false);
         repository.insert(testTransaction).blockingGet();
-        resultBalance = LiveDataTestUtil.getOrAwaitValue(
+        resultBalance = getOrAwaitValue(
                 repository.getBalanceFromMonth(1, DateUtil.now().minusMonths(2)));
-        // 597 (stays the same)
-        assertEquals(BigDecimalUtil.fromString("597"), resultBalance.getTotalIncomes());
-        // 739 (stays the same)
-        assertEquals(BigDecimalUtil.fromString("739"), resultBalance.getTotalExpenses());
-        // 992 (stays the same)
-        assertEquals(BigDecimalUtil.fromString("992"), resultBalance.getProjectedIncomes());
-        // 100 + 567 = 667
-        assertEquals(BigDecimalUtil.fromString("667"), resultBalance.getProjectedExpenses());
+        testResultBalance(resultBalance,
+                "597", // (stays the same)
+                "739", // (stays the same)
+                "992", // (stays the same)
+                "667"); // 100 + 567 = 667
 
         // Insert into a different balance (same account)
         testTransaction.setDate(DateUtil.OffsetDateTimeNow().minusMonths(1));
@@ -138,12 +122,13 @@ public class MoneyTransactionRepositoryTest {
         testTransaction.setConfirmed(true);
         repository.insert(testTransaction).blockingGet();
         // New balance is generated using provided transaction value
-        resultBalance = LiveDataTestUtil.getOrAwaitValue(
+        resultBalance = getOrAwaitValue(
                 repository.getBalanceFromMonth(1, DateUtil.now().minusMonths(1)));
-        assertEquals(BigDecimalUtil.fromString("567"), resultBalance.getTotalIncomes());
-        assertEquals(BigDecimalUtil.fromString("0"), resultBalance.getTotalExpenses());
-        assertEquals(BigDecimalUtil.fromString("0"), resultBalance.getProjectedIncomes());
-        assertEquals(BigDecimalUtil.fromString("0"), resultBalance.getProjectedExpenses());
+        testResultBalance(resultBalance,
+                "567", // (transaction value)
+                "0", // (initial value)
+                "0", // (initial value)
+                "0"); // (initial value)
 
         // Try to insert into faulty balance (invalid account id)
         testTransaction.setAccount(10);
@@ -167,5 +152,28 @@ public class MoneyTransactionRepositoryTest {
         // Test what happens with invalid accounts or categories (some error)
 
         assert true;
+    }
+
+    private void testResultBalance(MonthlyBalance resultBalance,
+                                  String expectedTotalIncomesString,
+                                  String expectedTotalExpensesString,
+                                  String expectedProjectedIncomesString,
+                                  String expectedProjectedExpensesString) {
+        assertEquals(BigDecimalUtil.fromString(expectedTotalIncomesString), resultBalance.getTotalIncomes());
+        assertEquals(BigDecimalUtil.fromString(expectedTotalExpensesString), resultBalance.getTotalExpenses());
+        assertEquals(BigDecimalUtil.fromString(expectedProjectedIncomesString), resultBalance.getProjectedIncomes());
+        assertEquals(BigDecimalUtil.fromString(expectedProjectedExpensesString), resultBalance.getProjectedExpenses());
+    }
+
+    private MoneyTransaction getDefaultTestTransaction() {
+        MoneyTransaction testTransaction = new MoneyTransaction();
+        testTransaction.setAccount(1);
+        testTransaction.setCategory(1);
+        testTransaction.setDate(DateUtil.OffsetDateTimeNow().minusMonths(2));
+        testTransaction.setCurrency("USD");
+        testTransaction.setOriginalValue("567");
+        testTransaction.setType(MoneyTransaction.INCOME);
+        testTransaction.setConfirmed(true);
+        return testTransaction;
     }
 }
